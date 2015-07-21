7567ddf86e9520ea0d46d5e3f2d6254bd946e25a
Filip Hanik <fhanik@pivotal.io>
2015-07-21 13:32:25 -0600

    Check that migration works, we will have to plug in the data source accessor differently, may need to change how that works in the UAA

--- src/main/java/org/cloudfoundry/identity/migration/DataSourceConfiguration.java
+++ src/main/java/org/cloudfoundry/identity/migration/DataSourceConfiguration.java
@@ -1,5 +1,6 @@
 package org.cloudfoundry.identity.migration;
 
+import com.googlecode.flyway.core.Flyway;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -26,5 +27,27 @@ public class DataSourceConfiguration {
         return DataSourceBuilder
             .create()
             .build();
+    }
+
+    public void configureFlyway(Flyway flyway, String type) {
+        flyway.setInitOnMigrate(true);
+        flyway.setLocations("classpath:org/cloudfoundry/identity/uaa/db/" + type);
+        flyway.setInitVersion("1.5.2");
+    }
+
+    @Bean
+    public Flyway mysqlFlyway() {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(mysqlDataSource());
+        configureFlyway(flyway, "mysql");
+        return flyway;
+    }
+
+    @Bean
+    public Flyway postgresqlFlyway() {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(postgresqlDataSource());
+        configureFlyway(flyway, "postgresql");
+        return flyway;
     }
 }
--- src/test/java/org/cloudfoundry/identity/migration/DataSourceConfigurationTest.java
+++ src/test/java/org/cloudfoundry/identity/migration/DataSourceConfigurationTest.java
@@ -1,9 +1,16 @@
 package org.cloudfoundry.identity.migration;
 
+import com.googlecode.flyway.core.Flyway;
+import com.googlecode.flyway.core.api.MigrationInfo;
+import com.googlecode.flyway.core.api.MigrationInfoService;
+import com.googlecode.flyway.core.api.MigrationState;
+import com.googlecode.flyway.core.api.MigrationVersion;
+import org.cloudfoundry.identity.uaa.db.DataSourceAccessor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -16,11 +23,16 @@ import static org.junit.Assert.assertNotNull;
 @SpringApplicationConfiguration(classes = Application.class)
 public class DataSourceConfigurationTest {
 
+    public static final String VERSION = "2.4.0";
+
     @Autowired
     private DataSource mysqlDataSource;
 
     @Autowired
     private DataSource postgresqlDataSource;
+
+    @Autowired
+    private ApplicationContext context;
 
     @Test
     public void testDataSourceCreation() {
@@ -33,6 +45,31 @@ public class DataSourceConfigurationTest {
     public void testDataSourceWorking() {
         assertEquals(1, new JdbcTemplate(mysqlDataSource).queryForInt("SELECT 1"));
         assertEquals(1, new JdbcTemplate(postgresqlDataSource).queryForInt("SELECT 1"));
+    }
+
+    @Test
+    public void check_that_schema_is_at_the_right_version() throws Exception {
+        Flyway mysqlflyway = context.getBean("mysqlFlyway", Flyway.class);
+        new DataSourceAccessor().setDataSource(mysqlDataSource);
+        mysqlflyway.migrate();
+        Flyway postgresqlflyway = context.getBean("postgresqlFlyway", Flyway.class);
+        postgresqlflyway.migrate();
+        new DataSourceAccessor().setDataSource(postgresqlDataSource);
+        //printMigrationService("mysql", mysqlflyway.info());
+        //printMigrationService("postgresql", postgresqlflyway.info());
+        MigrationInfo[] mysqlInfo = mysqlflyway.info().all();
+        MigrationInfo[] postgresqlInfo = postgresqlflyway.info().all();
+        MigrationVersion expectedVersion = MigrationVersion.fromVersion(VERSION);
+        assertEquals(expectedVersion, mysqlInfo[mysqlInfo.length-1].getVersion());
+        assertEquals(MigrationState.SUCCESS, mysqlInfo[mysqlInfo.length-1].getState());
+        assertEquals(expectedVersion, postgresqlInfo[postgresqlInfo.length-1].getVersion());
+        assertEquals(MigrationState.SUCCESS, postgresqlInfo[postgresqlInfo.length-1].getState());
+    }
+
+    public void printMigrationService(String prefix, MigrationInfoService service) {
+        for (MigrationInfo info : service.all()) {
+            System.out.println(prefix+" = [version:" + info.getVersion().getVersion()+ "; state:"+info.getState().getDisplayName()+"]");
+        }
     }
 
 }
